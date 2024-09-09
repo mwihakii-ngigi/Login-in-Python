b2d18357a31bff4431ce9516fa079b171426f5fa
MWIHAKI NGIGI <ngigileah92@gmail.com>
2024-09-09 23:47:52 +0300

    Update

--- project.py
+++ project.py
@@ -2,20 +2,27 @@ from tkinter import *
 root = Tk()
 root.geometry("500x300")
 
-Label(root, text="Python Registration Form", font="arial 15 bold").grid(row=0, column=3)
+def getvals(): 
+ print("Accepted")
 
+# Heading
+ Label(root, text="Python Registration Form", font="arial 15 bold").grid(row=0, column=3)
+
+# Field Name
 name = Label(root, text="Name")
 phone = Label(root, text="Phone")
 gender = Label(root, text="Gender")
 emergency = Label(root, text="Emergency")
 paymentmode = Label(root, text="Payment Mode")
 
+# Packing Fields
 name.grid(row=1, column=2)
 phone.grid(row=2, column=2)
 gender.grid(row=3, column=2)
 emergency.grid(row=4, column=2)
 paymentmode.grid(row=5, column=2)
 
+# Variables for storing data
 namevalue = StringVar
 phonevalue = StringVar
 gendervalue = StringVar
@@ -23,17 +30,27 @@ emergencyvalue = StringVar
 paymentmodevalue = StringVar
 checkvalue = IntVar
 
+# Creating entry field
 nameentry = Entry(root, textvariable=namevalue)
 phoneentry = Entry(root, textvariable=phonevalue)
 genderentry = Entry(root, textvariable=gendervalue)
 emergencyentry = Entry(root, textvariable=emergencyvalue)
 paymentmodeentry = Entry(root, textvariable=paymentmodevalue)
 
+# Packing entry field
 nameentry.grid(row=1, column=3)
 phoneentry.grid(row=2, column=3)
 genderentry.grid(row=3, column=3)
 emergencyentry.grid(row=4, column=3)
 paymentmodeentry.grid(row=5, column=3)
+
+# Creating Checkbox
+checkbtn = Checkbutton(text="Remember Me?", variable=checkvalue)
+checkbtn.grid(row=6, column=3)
+
+# Submit button
+Button(text="Submit", command=getvals).grid(row=7, column=3)
+
 
 
 
